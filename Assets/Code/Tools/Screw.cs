59f66f1af4917a9ea47c186168d5b7f7d32050bf
Tyler <ttrebuchon@live.com>
2018-11-27 12:52:58 -0500

    Work on screwdriver. Update power supply wires in RopeDev scene

---
+++ Assets/Code/Tools/Screw.cs
@@ -0,0 +1,15 @@
+﻿using HoloToolkit.Unity.InputModule.Examples.Grabbables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DCATS.Assets.Tools
+{
+    public class Screw : GrabbableSnapToOrient
+    {
+        [SerializeField]
+        public ScrewKind Kind = ScrewKind.Phillips;
+    }
+}
--- Assets/Code/Tools/Screwdriver.cs
+++ Assets/Code/Tools/Screwdriver.cs
@@ -9,8 +9,57 @@ namespace DCATS.Assets.Tools
 {
     public class Screwdriver : BaseUsable
     {
+        #region PropertyBackings
+
+        GameObject _tipPoint = null;
+        Grabber _tipGrabber = null;
+
+        #endregion
+
+
         [SerializeField]
         public ScrewKind Kind = ScrewKind.Phillips;
+
+        [SerializeField]
+        public float CutoffDistance = 0.5f;
+
+        protected GameObject AttachedScrew = null;
+
+        protected Transform TipTransform
+        {
+            get
+            {
+                if (_tipPoint == null)
+                {
+                    SetupTip();
+                }
+
+                return _tipPoint.transform;
+            }
+        }
+
+        protected Grabber TipGrabber
+        {
+            get
+            {
+                if (_tipGrabber == null)
+                {
+                    SetupTip();
+                }
+                return _tipGrabber;
+            }
+        }
+
+        private void SetupTip()
+        {
+            _tipPoint = this.gameObject.transform.Find("TipPoint").gameObject;
+            var tipGrabber = _tipPoint.GetComponent<Grabber>();
+            if (tipGrabber == null)
+            {
+                tipGrabber = _tipPoint.AddComponent<Grabber>();
+            }
+            this._tipGrabber = tipGrabber;
+        }
 
 
         public Screwdriver()
@@ -26,7 +75,7 @@ namespace DCATS.Assets.Tools
 
         protected override void OnEnable()
         {
-            //base.OnEnable();
+            base.OnEnable();
 
             Debug.Log("Screwdriver is ENABLED.");
         }
@@ -42,11 +91,97 @@ namespace DCATS.Assets.Tools
         protected override void UseStart()
         {
             Debug.Log("Screwdriver is being USED.");
+            var objs = this.gameObject.scene.GetRootGameObjects();
+            Screw closest = null;
+            float closestDistance = 0.0f;
+            var screws = Component.FindObjectsOfType(typeof(Screw)).OfType<Screw>();
+
+            foreach (var screw in screws)
+            {
+                Debug.Log("Found a screw!");
+                var dist = (screw.transform.position - this.transform.position).magnitude;
+                Debug.Log("Distance: " + dist.ToString());
+                if (screw.Kind == this.Kind)
+                {
+                    if (closest == null)
+                    {
+                        closest = screw;
+                        closestDistance = dist;
+                    }
+                    else
+                    {
+                        if (dist < closestDistance)
+                        {
+                            closestDistance = dist;
+                            closest = screw;
+                        }
+                    }
+                }
+            }
+
+            //foreach (var obj in objs)
+            //{
+            //    var comps = obj.GetComponentsInChildren<Screw>();
+            //    if (comps == null || comps.Length == 0)
+            //    {
+            //        continue;
+            //    }
+
+            //    foreach (var screw in comps)
+            //    {
+            //        Debug.Log("Found a screw!");
+            //        var dist = (screw.transform.position - this.transform.position).magnitude;
+            //        Debug.Log("Distance: " + dist.ToString());
+            //        if (screw.Kind == this.Kind)
+            //        {
+            //            if (closest == null)
+            //            {
+            //                closest = screw;
+            //                closestDistance = dist;
+            //            }
+            //            else
+            //            {
+            //                if (dist < closestDistance)
+            //                {
+            //                    closestDistance = dist;
+            //                    closest = screw;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            
+
+            if (closest != null && closestDistance <= CutoffDistance)
+            {
+                Debug.Log("Attaching screw...");
+                AttachScrew(closest);
+            }
+            else
+            {
+                Debug.Log("Too far away or no screw found. Closest screw: " + closestDistance.ToString());
+            }
         }
 
         protected override void UseEnd()
         {
             Debug.Log("Screwdriver is DONE being USED.");
+        }
+
+
+        protected void AttachScrew(Screw screw)
+        {
+            var grabbable = screw.GetComponent<BaseGrabbable>();
+            if (!grabbable.TryGrabWith(TipGrabber))
+            {
+                Debug.Log("Couldn't grab the screw.");
+                // TODO
+            }
+            else
+            {
+                Debug.Log("Grabbed the screw!!!");
+            }
         }
     }
 }
