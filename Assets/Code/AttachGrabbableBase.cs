419c4d744bfa8579c60f67af1da90728b1f38a8d
Tyler <ttrebuchon@live.com>
2018-11-28 05:58:41 -0500

    Create base classes/templates for attaching components via grabber mechanics

---
+++ Assets/Code/AttachGrabbableBase.cs
@@ -0,0 +1,30 @@
+﻿using HoloToolkit.Unity.InputModule.Examples.Grabbables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DCATS.Assets
+{
+    public class AttachGrabbableBase : GrabbableSnapToOrient
+    {
+        public virtual void DisableGrabbing()
+        {
+            this.enabled = false;
+        }
+
+        public virtual void EnableGrabbing()
+        {
+            this.enabled = true;
+        }
+
+        public virtual void DetachAllGrabbers()
+        {
+            foreach (var grabber in this.ActiveGrabbers)
+            {
+                this.DetachFromGrabber(grabber);
+            }
+        }
+    }
+}
---
+++ Assets/Code/AttachGrabberBase.cs
@@ -0,0 +1,35 @@
+﻿using HoloToolkit.Unity.InputModule.Examples.Grabbables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DCATS.Assets
+{
+    public class AttachGrabberBase : Grabber
+    {
+        protected override void OnEnable()
+        {
+
+        }
+
+        protected override void OnDisable()
+        {
+            grabbedObjects.Clear();
+        }
+
+        public void DoGrab(BaseGrabbable obj)
+        {
+            if (obj.TryGrabWith(this))
+            {
+                this.grabbedObjects.Add(obj);
+            }
+        }
+
+        public void FinishGrab()
+        {
+            this.GrabEnd();
+        }
+    }
+}
