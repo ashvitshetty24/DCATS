97ae136596cac2994a0f8cee7ec74a2fe035716d
Tyler <ttrebuchon@live.com>
2018-12-02 18:19:55 -0500

    Add script-tool to move box colliders to child objects (For obi colliders)

---
+++ Assets/Editor/CustomBoxColliderMove.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using DCATS.Assets.Utility;
+
+namespace DCATS.Assets.Editor
+{
+#if UNITY_EDITOR
+    [CustomEditor(typeof(CustomBoxColliderMove))]
+    public class CustomBoxColliderMoveEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Calculate"))
+            {
+                var script = target as CustomBoxColliderMove;
+                if (script != null)
+                {
+                    script.Go();
+                }
+            }
+        }
+    }
+#endif
+}
