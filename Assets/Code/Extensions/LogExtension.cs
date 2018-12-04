0734a744e8491254b22f1c24af47fea48a1c1b94
Tyler <ttrebuchon@live.com>
2018-12-04 16:48:55 -0500

    Add UnityEngine.Object.ObjectLog() extension method

---
+++ Assets/Code/Extensions/LogExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DCATS.Assets.Extensions
+{
+    public static class LogExtension
+    {
+        public static void ObjectLog(this UnityEngine.Object obj, object message)
+        {
+            Debug.Log("[" + obj.name + "] " + message, obj);
+        }
+    }
+}
